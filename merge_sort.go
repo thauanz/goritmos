4bdc09a9944a089d89ea4882e9c15a2584edfa40
Thauan Zatta <thauanz@gmail.com>
2017-07-28 18:51:41 -0300

    Added merge sort

---
+++ merge_sort.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{14, 33, 27, 10, 35, 19, 42, 44, 7}
+	fmt.Println("Before:", numbers)
+	fmt.Println("After:", mergeSort(numbers))
+}
+
+func mergeSort(numbers []int) []int {
+	qtd := len(numbers)
+
+	if qtd == 1 {
+		return numbers
+	}
+	mid := qtd / 2
+
+	l := numbers[0:mid]
+	r := numbers[mid:qtd]
+
+	return merge(mergeSort(l), mergeSort(r))
+}
+
+func merge(l, r []int) []int {
+	var result = make([]int, len(l)+len(r))
+
+	x := 0
+	for len(l) > 0 && len(r) > 0 {
+		if l[0] < r[0] {
+			result[x] = l[0]
+			l = l[1:] // remove the list
+		} else {
+			result[x] = r[0]
+			r = r[1:] // remove the list
+		}
+		x++
+	}
+
+	for i := 0; i < len(l); i++ {
+		result[x] = l[i]
+		x++
+	}
+	for i := 0; i < len(r); i++ {
+		result[x] = r[i]
+		x++
+	}
+
+	return result
+}
